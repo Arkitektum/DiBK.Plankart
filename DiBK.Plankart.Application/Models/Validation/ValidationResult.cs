3c71669bb3f2dfd0152eae382728193ca3281fa3
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2021-10-29 11:10:35 +0200

    Finpussing

--- DiBK.Plankart.Application/Models/Validation/ValidationResult.cs
+++ DiBK.Plankart.Application/Models/Validation/ValidationResult.cs
@@ -6,6 +6,7 @@ namespace DiBK.Plankart.Application.Models.Validation
 {
     public class ValidationResult
     {
+        public Guid Id { get; } = Guid.NewGuid();
         public IEnumerable<ValidationRule> Rules { get; set; } = Array.Empty<ValidationRule>();
         public IEnumerable<string> XsdValidationMessages { get; set; } = Array.Empty<string>();
         public bool XsdValidated => !XsdValidationMessages.Any();
